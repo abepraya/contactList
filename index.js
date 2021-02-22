d3254e0c380e47af3eca728660ddfbb85f98e574
Muhammad Jadid Abi Praya <muhammadjadidabipraya@gmail.com>
2021-02-22 13:16:25 +0700

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import AddUser from './screen/AddUser/AddUser';
+// import AddUser from './screen/AddUser/AddUser';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => AddUser);
+AppRegistry.registerComponent(appName, () => App);
